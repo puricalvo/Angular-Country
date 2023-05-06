3cb1219d8b5afcda1e18f574c015528cd224b61c
cadesa68 <puricld33@gmail.com>
2023-05-06 07:45:22 +0200

    Fin aplicacion Country

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent,
+    SidebarComponent,
+    ContactPageComponent,
+    SearchBoxComponent
+  ],
+
+  exports: [
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SearchBoxComponent,
+    SidebarComponent,
+  ],
+
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class SharedModule { }
